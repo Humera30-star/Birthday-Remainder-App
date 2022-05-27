e90ebdfdeccd120bcc4ffb9e07f1885f102a2138
Humera30-star <amirhamzamujawar73@gmail.com>
2022-05-27 19:59:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import {data} from "./Birthday";
+
+function App() {
+  const [people,setPeople]=useState(data)
+  const removePerson=(id) =>{
+    let newPerson=people.filter((person) => person.id!==id)
+    setPeople(newPerson)
+  }
+  return (
+    <>
+    <h3 style={{backgroundColor:"yellow",color:"purple",textAlign:"center"}}> {people.length} Birthday's Today</h3>
+  
+    {people.map((person) => {const{id,name,image}=person
+    return(
+      <div className="container" key={id}>
+        <div class="pic">
+          <img src ={image} alt={name} />
+          <h3>{name}</h3>
+        </div>
+        <div className="button">
+          <button className="btn" onClick={() => removePerson(id)} >DISMISS</button>
+        </div>
+      </div>
+    )
+    
+})}
+
+    </>
+  );
+}
+
+export default App;
---
+++ Birthday.js
@@ -0,0 +1,53 @@
+export const data=[
+    {
+        id:1,
+        name:"Riya jain",
+        image:" https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1494790108377-be9c29b29330%3Fixlib%3Drb-1.2.1%26ixid%3DMnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%253D%26w%3D1000%26q%3D80&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fprofile&tbnid=-H_ExttvR7ahYM&vet=12ahUKEwjbksOt6v_3AhWUDbcAHae7DxsQMygGegUIARDjAQ..i&docid=b5C9ViMmmhpq-M&w=1000&h=1500&q=profile%20picture&ved=2ahUKEwjbksOt6v_3AhWUDbcAHae7DxsQMygGegUIARDjAQ "
+    },
+    {
+        id:2,
+        name:"John Rask",
+        image:" https://www.google.com/search?q=profile+picture&rlz=1C1GCEA_enIN941IN941&sxsrf=ALiCzsZGT3894oOeFwQ2Nw58rM5U1Mf0dA:1653488370460&tbm=isch&source=iu&ictx=1&vet=1&fir=nfkyptoYx2OzJM%252CFvQHUVZ-cx81xM%252C_%253BZbfgeaptF8Y5ZM%252CPumo7KynxbL-EM%252C_%253BXzC2iXNqhm0dvM%252CFvQHUVZ-cx81xM%252C_%253BK3TYQSDHIhDw4M%252CFvQHUVZ-cx81xM%252C_%253BMc-JhW0xZaOx2M%252CfyERMF43Twp-LM%252C_%253BkevY2s_U4GhnjM%252C_wo3Wk6WMCa34M%252C_%253BWgxrqZ8j04F8BM%252Cw0rekurKV2OhcM%252C_%253B8z0GwDReiUhJ6M%252CMS0735Gr0ZdLzM%252C_%253BQsatBu9zpFWl5M%252CFvQHUVZ-cx81xM%252C_%253B6RmIrzFFfd0sLM%252CNWVUfzuKBXde5M%252C_%253BioYoSGPe9jA2IM%252C_lgUfa9iUIcUwM%252C_%253Blcjtk8drNcGL8M%252COgWk3wP_2xVHMM%252C_%253BxYE_STtP-rI6NM%252CcDBMf-N2RFeabM%252C_%253B509r_97i2bOK4M%252CVzvDEJCSzpTLLM%252C_%253BTr9Q-gojDjAbVM%252CZCaZPb4h69Op6M%252C_%253Bt7RHuEm7KCCQlM%252CQikDQKcDdNuTsM%252C_%253B4vd3wkog6Ao2HM%252CSWtDHZ9YsH0kUM%252C_%253B8uAQ9db6UXFknM%252CLzzbH24EyrVRkM%252C_%253BCp7xX7V_43GszM%252CqMAiiO3p_kaY5M%252C_&usg=AI4_-kSBBXlLNbr7Kl8r79pD9TBRxDGoeQ&sa=X&ved=2ahUKEwjPmtiX7Pr3AhU8T2wGHZ_RAJoQ9QF6BAgXEAE#imgrc=K3TYQSDHIhDw4M"
+    },
+    {
+        id:3,
+        name:"Samaira",
+        image:" https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.unigreet.com%2Fwp-content%2Fuploads%2F2020%2F04%2FSweet-girl-dp.jpg&imgrefurl=https%3A%2F%2Fwww.unigreet.com%2Fsweet-whatsapp-profile-images%2F&tbnid=VdCiJi52G6fDJM&vet=12ahUKEwiChIKn7fr3AhUkT3wKHf5bA_UQMyg5egQIARBr..i&docid=2tmRh42XnG1bpM&w=1080&h=1054&q=profile%20picture&ved=2ahUKEwiChIKn7fr3AhUkT3wKHf5bA_UQMyg5egQIARBr"
+    },
+    {
+        id:4,
+        name:"Shadan khan",
+        image:" https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Fpleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg%3Fw%3D2000&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fface-profile&tbnid=D6gTFSDWaohX4M&vet=10CFMQMyiNAWoXChMI6Pipku769wIVAAAAAB0AAAAAEAI..i&docid=6F3KtEn3aVkDRM&w=2000&h=1333&q=profile%20picture&ved=0CFMQMyiNAWoXChMI6Pipku769wIVAAAAAB0AAAAAEAI"
+    },
+    {
+        id:5,
+        name:"Rohit",
+        image:"https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.goodmorningimagesdownload.com%2Fwp-content%2Fuploads%2F2021%2F07%2F1080p-New-Cool-Whatsapp-Dp-Profile-Images-pictures-hd-1-300x300.jpg&imgrefurl=http%3A%2F%2Fwww.goodmorningimagesdownload.com%2Fcool-whatsapp-dp%2F&tbnid=I7DENtYAYyVToM&vet=12ahUKEwiChIKn7fr3AhUkT3wKHf5bA_UQMyhkegUIARDNAQ..i&docid=mHdO4IZTpov3TM&w=300&h=300&q=profile%20picture&ved=2ahUKEwiChIKn7fr3AhUkT3wKHf5bA_UQMyhkegUIARDNAQ " 
+    },
+    {
+        id:6,
+        name:"Osman",
+        image:" https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F785%2F0*Ggt-XwliwAO6QURi.jpg&imgrefurl=https%3A%2F%2Fmedium.com%2F%40KaitlinZhang%2Ftop-11-tips-to-take-the-perfect-profile-photo-with-example-portraits-55b9e1950b0&tbnid=zCCgGFOWLagTBM&vet=12ahUKEwiChIKn7fr3AhUkT3wKHf5bA_UQMyg8egQIARBx..i&docid=6CVZdROVq4QIQM&w=785&h=1165&q=profile%20picture&ved=2ahUKEwiChIKn7fr3AhUkT3wKHf5bA_UQMyg8egQIARBx" 
+    },
+    {
+        id:7,
+        name:"Bamsi",
+        image:" https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic-cse.canva.com%2Fblob%2F835514%2F1600w-YTfEMXMuMCs.jpg&imgrefurl=https%3A%2F%2Fwww.canva.com%2Fcreate%2Fprofile-pictures%2F&tbnid=jm-bSNYR_4Y9sM&vet=10CHkQMyidAWoXChMI6Pipku769wIVAAAAAB0AAAAAEAI..i&docid=NZyXXJTBgZi2mM&w=1600&h=1600&q=profile%20picture&ved=0CHkQMyidAWoXChMI6Pipku769wIVAAAAAB0AAAAAEAI" 
+    },
+    {
+        id:8,
+        name:"Joy",
+        image:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fwww.kahanihindi.com%2Fwp-content%2Fuploads%2F2020%2F02%2FWhatsapp-DP-HALF-SIZE-Profile-81.jpg%3Fresize%3D500%252C500%26ssl%3D1&imgrefurl=https%3A%2F%2Fwww.kahanihindi.com%2Fsweet-images-fb-and-whatsapp-dp-profile%2F&tbnid=7yBJAnfqgfBROM&vet=10CJUBEDMoqwFqFwoTCOj4qZLu-vcCFQAAAAAdAAAAABAC..i&docid=LNlr-VwpsriosM&w=500&h=500&q=profile%20picture&ved=0CJUBEDMoqwFqFwoTCOj4qZLu-vcCFQAAAAAdAAAAABAC " 
+    },
+    {
+        id:9,
+        name:"Rancho",
+        image:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.shutterstock.com%2Fimage-photo%2Fprofile-picture-smiling-millennial-asian-260nw-1836020740.jpg&imgrefurl=https%3A%2F%2Fstocksnap.io%2Fsearch%2Fprofile&tbnid=-h20Jdis7Qx6mM&vet=10CMkBEDMowgFqFwoTCOj4qZLu-vcCFQAAAAAdAAAAABAC..i&docid=-m0v_GLbqugcoM&w=390&h=280&q=profile%20picture&ved=0CMkBEDMowgFqFwoTCOj4qZLu-vcCFQAAAAAdAAAAABAC " 
+    },
+    {
+        id:10,
+        name:"Farhan",
+        image:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fexpertphotography.b-cdn.net%2Fwp-content%2Fuploads%2F2020%2F08%2Fsocial-media-profile-photos-3.jpg&imgrefurl=https%3A%2F%2Fexpertphotography.com%2Fcool-profile-pictures%2F&tbnid=fzulRkMgPKOk_M&vet=10CGQQMyj8AWoXChMI6Pipku769wIVAAAAAB0AAAAAEAM..i&docid=IFzJeadVXkgROM&w=700&h=467&q=profile%20picture&ved=0CGQQMyj8AWoXChMI6Pipku769wIVAAAAAB0AAAAAEAM " 
+    },
+    
+]
